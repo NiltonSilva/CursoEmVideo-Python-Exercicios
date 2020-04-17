c84181d9f112c8649a7c8c6d727e68ee736bbd7e
Nilton Silva <nsfgeo@hotmail.com>
2020-04-17 20:52:26 -0300

    Exercícios 25 e 26 do curso de Python

---
+++ ex025.py
@@ -0,0 +1,3 @@
+nome = str(input('Informe o nome: ')).strip() # ".sprit() tira os espaços de antes e depois.
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower())) # "in" é um operador para saber se tem algo dentro da constante.
+
